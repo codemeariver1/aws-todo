09d394211eedca9fc0131c3c2c22fda130f3cf54
codemeariver1 <91358795+gucciflipflops1@users.noreply.github.com>
2022-10-12 03:57:51 -0400

    init commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+    "hosting": {
+        "S3AndCloudFront": {
+            "Region": "string",
+            "HostingBucketName": "string",
+            "WebsiteURL": "string",
+            "S3BucketSecureURL": "string"
+        }
+    },
+    "auth": {
+        "awstodo03d4e39f": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "awstodo": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { Amplify, Auth, API, graphqlOperation } from 'aws-amplify'
+import awsconfig from './aws-exports'
+import { withAuthenticator, Button } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css';
+import * as queries from './graphql/queries'
+import * as mutations from './graphql/mutations'
 
-function App() {
+Amplify.configure(awsconfig)
+API.configure(awsconfig)
+
+function updateTodo(todo, newDesc) {
+  todo['description'] = newDesc
+  todo['createdAt'] = undefined
+  todo['updatedAt'] = undefined
+  todo['owner'] = undefined
+  API.graphql(graphqlOperation(mutations.updateTodo, {input: todo})).catch(err => console.log(err))
+}
+
+function deleteTodo(todo) {
+  API.graphql(graphqlOperation(mutations.deleteTodo, {input:{'id':todo['id']}}))
+}
+
+function App({ user, signOut }) {
+
+  const allTodos = API.graphql(graphqlOperation(queries.listTodos))
+  console.log(allTodos)
+
+  /*const oneTodo = API.graphql(graphqlOperation(queries.getTodo, {id:"64c62c89-f72a-4ad3-9bc5-ba624d0b68a9"})).then(function(todo) {
+    updateTodo(todo['data']['getTodo'], "new desc")
+    deleteTodo(todo['data']['getTodo'])
+  })
+  console.log(oneTodo)*/
+
+  const filteredTodos = API.graphql(graphqlOperation(queries.listTodos, {filter:{'name':{eq:"app"}}})).catch(err => console.log(err))
+  console.log(filteredTodos)
+
+  /*Auth.currentAuthenticatedUser({
+    bypassCache: false
+  }).then(function(user) {
+    console.log("User: " + JSON.stringify(user))
+  })
+  const todo = {name: user['username'], description: "new todo"}
+  const newTodo = API.graphql(graphqlOperation(mutations.createTodo, {input: todo})).catch(err => console.log(err))*/
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Happy Hacking Bois!
         </p>
         <a
           className="App-link"
@@ -16,10 +58,12 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> 
+        <Button onClick={signOut}>Sign Out</Button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default withAuthenticator(App, {includeGreetings: true})
+//export default App;
